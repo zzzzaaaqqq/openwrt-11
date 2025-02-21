32b5db0bf560a0d754162a1197069bbf120ab260
zzzzaaaqqq <96756238+zzzzaaaqqq@users.noreply.github.com>
2025-02-22 01:55:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F​
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
